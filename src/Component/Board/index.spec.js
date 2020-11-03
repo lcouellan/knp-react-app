097b53b56d53b5c7715cb6a39c8becd48a0dad1f
Lenaic <lcouellan@gmail.com>
2020-11-03 14:11:29 +0100

    fix: review comments

--- src/Component/Board/index.spec.js
+++ src/Component/Board/index.spec.js
@@ -1,24 +1,42 @@
 import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import TestRenderer, { act } from 'react-test-renderer'
 import Board from './index'
 import State from '../../Redux/State'
-import TestRenderer from 'react-test-renderer'
+import { addCard, receiveCard } from '../../Redux/State/Board'
+import { createContainer } from '../../utils-test'
 
 describe('Component :: Board :: index', () => {
 
   it('checks the initial state and verify if the board is empty', () => {
     const store = createStore(State, State(), undefined)
-    const inspector = TestRenderer.create(createElement(store, Board))
+    let inspector = TestRenderer.create(createElement(store, Board))
 
     inspector.root.findByProps({ className: 'myBoard' })
+
+    act(() => {
+      inspector = createContainer(<Board />, store)
+    })
+
+    expect(inspector).toMatchSnapshot()
   })
 
   it('loads when a card is creating', () => {
     const store = createStore(State, State({ Board: { isLoading: true, cards: [] } }), undefined)
-    const inspector = TestRenderer.create(createElement(store, Board))
+    let inspector = TestRenderer.create(createElement(store, Board))
 
     inspector.root.findByProps({ className: 'board loader' })
+
+    act(() => {
+      inspector = createContainer(<Board />, store)
+    })
+
+    act(() => {
+      store.dispatch(addCard())
+    })
+
+    expect(inspector).toMatchSnapshot()
   })
 
   it('displays the cards and add "add another card button" when it is not creating any card', () => {
@@ -27,10 +45,20 @@ describe('Component :: Board :: index', () => {
       State({ Board: { isLoading: false, cards: [{ title: 'My title' }] } }),
       undefined
     )
-    const inspector = TestRenderer.create(createElement(store, Board))
+    let inspector = TestRenderer.create(createElement(store, Board))
 
     inspector.root.findByProps({ className: 'board add-card' })
     inspector.root.findByProps({ className: 'card-title' })
+
+    act(() => {
+      inspector = createContainer(<Board />, store)
+    })
+
+    act(() => {
+      store.dispatch(receiveCard({ title: 'My test card' }))
+    })
+
+    expect(inspector).toMatchSnapshot()
   })
 })
 
